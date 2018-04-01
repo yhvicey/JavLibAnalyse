25685c201ee0c1a56eab5d3d1732ec3b6be7654a
Vicey Wang <vicey@live.com>
2018-04-01 18:57:25 +0800

    Add checkpoint for ProducerTasks

--- Crawler/Dispatcher.cs
+++ Crawler/Dispatcher.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Crawler
 {
@@ -22,7 +24,7 @@ namespace Crawler
         {
             lock (ProducerSyncLock)
             {
-                if (ProducerTaskHistory.Contains((genres, page)))
+                if (IsProducerTaskFinished(genres, page))
                     return;
                 ProducerTasks.Add((genres, page));
             }
@@ -30,10 +32,13 @@ namespace Crawler
 
         public static void FinishProducerTask(string genres, int page)
         {
+            if (IsProducerTaskFinished(genres, page)) return;
             lock (ProducerSyncLock)
             {
-                ProducerTaskHistory.Add((genres, page));
+                ProducerTaskHistory[genres] = page;
             }
+            if (++CheckpointCounter >= Config.CheckpointThreshold)
+                Checkpoint();
         }
 
         public static string GetProcessorTask()
@@ -44,6 +49,13 @@ namespace Crawler
 
         public static bool IsProcessorTaskFinished(string task)
             => Directory.Exists($"{Config.OutputDir}/{task}");
+
+        public static bool IsProducerTaskFinished(string genres, int page)
+        {
+            return ProducerTaskHistory.ContainsKey(genres)
+                ? ProducerTaskHistory[genres] >= page
+                : false;
+        }
 
         public static void PrintInfo()
         {
@@ -64,7 +76,44 @@ namespace Crawler
         private static readonly object ProcessorSyncLock = new object();
         private static readonly BlockingCollection<string> ProcessorTasks = new BlockingCollection<string>();
         private static readonly object ProducerSyncLock = new object();
-        private static readonly HashSet<(string, int)> ProducerTaskHistory = new HashSet<(string, int)>();
+        private static readonly Dictionary<string, int> ProducerTaskHistory = new Dictionary<string, int>();
         private static readonly BlockingCollection<(string, int)> ProducerTasks = new BlockingCollection<(string, int)>();
+
+        private static int CheckpointCounter = 0;
+
+        static Dispatcher()
+        {
+            var checkpointFiles = Directory.GetFiles(Config.TempDir, "checkpoint_*").ToList();
+            if (checkpointFiles.Count == 0) return;
+            checkpointFiles.Sort();
+            var checkpointFile = checkpointFiles.Last();
+            try
+            {
+                foreach (var record in File.ReadAllLines(checkpointFile))
+                {
+                    var sessions = record.Split(",");
+                    ProducerTaskHistory.Add(sessions.First(), int.Parse(sessions.Last()));
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"Failed to load checkpoint file. File path: {checkpointFile}.", ex);
+            }
+        }
+
+        private static void Checkpoint()
+        {
+            CheckpointCounter = 0;
+            var checkpointFilePath = $"{Config.TempDir}/checkpoint_{DateTime.Now:yyyy_MM_dd_HH_mm_ss_ffff}";
+            try
+            {
+                File.WriteAllLines(checkpointFilePath, ProducerTaskHistory.Select(record => $"{record.Key},{record.Value}"));
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"Failed to create checkpoint file. File path: {checkpointFilePath}.", ex);
+            }
+        }
+
     }
 }
